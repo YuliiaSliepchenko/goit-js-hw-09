bf37cd7eff9d68802332e9e7244db9aab9b15243
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-03-24 23:07:26 +0200

    get color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+
+let timerId = null;
+
+
+  function changeColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+   
+    startBtn.disabled = true;
+  }
+
+  function stopChangeColor(){
+    clearInterval(timerId);
+
+    startBtn.disabled = false;
+
+  }
+
+  startBtn.addEventListener("click", () => timerId = setInterval(changeColor,1000));
+
+
+
+stopBtn.addEventListener("click",stopChangeColor);
 
