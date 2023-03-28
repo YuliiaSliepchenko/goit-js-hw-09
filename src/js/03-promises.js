1db8142ac989261e1c41f333be72d2869fed9c34
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-03-28 23:55:25 +0300

    hw3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css'
+
+const refs = {form : document.querySelector('form'),
+}
+
+
+
+refs.form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const values = {
+  amount:Number(evt.target.elements.amount.value),
+  firstDelay:Number(evt.target.elements.delay.value),
+  stepDelay:Number(evt.target.elements.step.value),
+};
+let {amount,  firstDelay,  stepDelay } = values;
+for(i = 1; i <= amount; i +=1){
+
+  createPromise(i, firstDelay)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  firstDelay += stepDelay;  
+}
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+return new Promise((resolve, reject) => { 
+  setTimeout(()=> { 
+    if (shouldResolve) {
     // Fulfill
+    resolve({position, delay})
   } else {
     // Reject
-  }
+    reject({position , delay});
+  }},delay)
+ });
+ 
 }
+
