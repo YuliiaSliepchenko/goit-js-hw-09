61b7eb5169e389f755188a9e82af0a3c5cd98f9c
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-03-31 20:20:05 +0000

    Deploying to gh-pages from @ YuliiaSliepchenko/goit-js-hw-09@315d90fb8912b056c05880bead4ceb8c8b4ac7ec 🚀

--- 01-color-switcher.f460291d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}t.addEventListener("click",(function(){return n=setInterval(a,1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.f460291d.js.map
